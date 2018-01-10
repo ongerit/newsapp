837f8e692ad17cffec46507346b320fd5687e5f4
ongerit@gmail.com <ongerit@gmail.com>
2018-01-10 08:56:11 -0500

    added a placeholder

--- components/Layout/Item.js
+++ components/Layout/Item.js
@@ -2,6 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import tz from 'moment-timezone';
 import s from './Item.css';
+import Placeholder from './Placeholder';
 
 class Item extends React.Component {
 
@@ -14,22 +15,26 @@ class Item extends React.Component {
   }
 
   render() {
-
     const article = this.props.article;
     const source = this.props.source;
+    console.log(article);
     let backgroundImg = article.urlToImage || '/bg.jpg';
-    
-    if(source === undefined) {
-      return;
+
+    if (source === undefined) {
+      return
     }
 
     return (
-      <section style={{backgroundImage: `url(${backgroundImg})`}} className={`mdl-layout__item ${s.item}`} ref={node => (this.root = node)}>
-        <div>
-          <a title={source.name} href={source.url}>{source.name}</a>
-	  <a href={source.url} title={source.description} ><h6 className={`${s.title}`}>{article.title}</h6></a>
-        </div>
-      </section>
+        <section style={{
+          backgroundImage: `url(${backgroundImg})`
+        }} className={`mdl-layout__item ${s.item}`} ref={node => (this.root = node)}>
+          <div>
+            <a title={source.name} href={source.url}>{source.name}</a>
+            <a href={source.url} title={source.description}>
+              <h6 className={`${s.title}`}>{article.title}</h6>
+            </a>
+          </div>
+        </section>
     );
   }
 }
--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,45 +1,45 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import _ from 'lodash';
 import Header from './Header';
 import Footer from '../Footer';
 import Item from './Item';
 import s from './Layout.css';
 import sources from './services/sources.json';
-import data2 from './services/data.json';
+// Placeholder data
+// import data2 from './services/data.json';
 import { getNews, getSources } from './services/newsapi';
 
 class Layout extends React.Component {
 
   static propTypes = {
-    className: PropTypes.string,
+    className: PropTypes.string
   }
 
   constructor(props) {
     super(props);
     this.state = {
       src: sources,
-      news: data2,
+      news: '',
       id: '',
-      counter: 0,
+      counter: 0
     };
     // init
     this.newsData();
   }
 
   componentWillMount() {
-    //this.newsData();
+    this.newsData();
   }
 
-  removeNewselements(){
+  removeNewselements() {
     const articlesTotalEndpoint = 2;
     const element = this.state.newsA.articles;
     let updateNew = element.slice(0, -1);
-    let updateNews = Object.assign({}, this.state.news, {articles:updateNew});
-    this.setState({news:updateNews});
+    let updateNews = Object.assign({}, this.state.news, {articles: updateNew});
+    this.setState({news: updateNews});
     // [TO] add new articles once
     // list of articles is less than 2
-
-    if(element.length < articlesTotalEndpoint ) {
+    if (element.length < articlesTotalEndpoint) {
       this.newsData();
     }
   }
@@ -49,17 +49,18 @@ class Layout extends React.Component {
       let articleIdArray = [];
       // [TO] user alternation to for in
       // https://github.com/airbnb/javascript/issues/851
-      for(let item in e) {
+      for (let item in e) {
         articleIdArray.push(e[item].id);
       }
       let numberOfArticles = e.length;
       let sourceArticle = articleIdArray[_.random(0, numberOfArticles)];
-      this.setState({ id: sourceArticle });
-      return sourceArticle;   
-    }).then( (source) => {
-	getNews(source)
-         .then(articles => { console.log(articles); this.setState({ news: articles })})
-         .catch(error => console.log(error));
+      this.setState({id: sourceArticle});
+      return sourceArticle;
+    }).then((source) => {
+      getNews(source).then(articles => {
+        console.log(articles);
+        this.setState({news: articles})
+      }).catch(error => console.log(error));
     })
   }
 
@@ -67,30 +68,35 @@ class Layout extends React.Component {
     const data = this.state.news;
     const src = this.state.src.sources;
     const origin = data.source;
-    
-   if(data.status === undefined) {
+    if (data.status === undefined) {
       this.newsData;
       return
     }
 
-    let src1 = _.find(src, (o) => { return o.id === origin});
-
+    let src1 = _.find(src, (o) => {
+      return o.id === origin
+    });
     return (
-      <div className="mdl-layout mdl-js-layout">
-        <div className="mdl-layout__inner-container">
-          <Header getNewsEvent={this.removeNewselements.bind(this)} />
-          <main className={`mdl-layout__content ${s.content}`}>
-            <div className={`${s.col}`}>
-              <div className={`pp-wrapper ${s.wrapper}`}>
-                {data.articles.map((article, i) => {
-                  return (<Item origin={origin} key = {i}  article={article} source={src1} />);
-                })}
-              </div>
-              </div>
-            <Footer />
-          </main>
-        </div>
-      </div>
+      {
+        if(data) {
+          <div className="mdl-layout mdl-js-layout">
+            <div className="mdl-layout__inner-container">
+              <Header getNewsEvent={this.removeNewselements.bind(this)}/>
+              <main className={`mdl-layout__content ${s.content}`}>
+                <div className={`${s.col}`}>
+                  <div className={`pp-wrapper ${s.wrapper}`}>
+                    { data.articles.map((article, i) => {
+                        return (<Item origin={origin} key={i} article={article} source={src1}/>);
+                      })
+                      }
+                  </div>
+                </div>
+                <Footer/>
+              </main>
+            </div>
+          </div>
+        }
+      }
     );
   }
 }
